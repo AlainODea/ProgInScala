66cdb93390dd2afa7474b4b222e7d5ed1bd4af9c
Alain O'Dea <alain@alain-laptop.(none)>
2011-05-03 21:13:47 -0230

    Chapter 12 in progress
     - Use stackable traits to yield much better factored code

--- src/main/scala/stackable.scala
+++ src/main/scala/stackable.scala
@@ -7,14 +7,7 @@ trait CharSequence {
 
 class Point(val x: Int, val y: Int)
 
-class Rectangle(val topLeft: Point, val bottomRight: Point) {
-  def left = topLeft.x
-  def right = bottomRight.x
-  def width = right - left
-  // and many more geometric methods...
-}
-
-abstract class Component {
+trait Rectangular {
   def topLeft: Point
   def bottomRight: Point
 
@@ -22,4 +15,13 @@ abstract class Component {
   def right = bottomRight.x
   def width = right - left
   // and many more geometric methods...
+}
+
+abstract class Component extends Rectangular {
+  // other methods...
+}
+
+class Rectangle(val topLeft: Point, val bottomRight: Point)
+    extends Rectangular {
+  // other methods...
 }
