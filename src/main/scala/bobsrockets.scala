26a7c3acb0b60240f62629ef2a81e669697abb54
Alain O'Dea <alain@alain-laptop.(none)>
2011-05-03 21:48:58 -0230

    Chapter 13 in progress
     - Separate package scopes in separate files with naming resolution

--- src/main/scala/bobsrockets.scala
+++ src/main/scala/bobsrockets.scala
@@ -1,10 +1,15 @@
 package bobsrockets {
-  class Ship
-}
-
-package bobsrockets.fleets {
-  class Fleet {
-    // Doesn't compile! Ship not in scope.
-    def addShip() { new Ship }
+  package navigation {
+    package launch {
+      class Booster1
+    }
+    class MissionControl {
+      val booster1 = new launch.Booster1
+      val booster2 = new bobsrockets.launch.Booster2
+      val booster3 = new _root_.launch.Booster3
+    }
+  }
+  package launch {
+    class Booster2
   }
 }
