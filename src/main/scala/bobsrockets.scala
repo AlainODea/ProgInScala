b36d0185a1b7db772b67d47d72152a7317c9f75f
Alain O'Dea <alain@alain-laptop.(none)>
2011-05-03 21:41:24 -0230

    Chapter 13 in progress
     - Nested packages in single file

--- src/main/scala/bobsrockets.scala
+++ src/main/scala/bobsrockets.scala
@@ -1,3 +1,13 @@
-package bobsrockets.navigation {
-  class Navigation
+package bobsrockets {
+  package navigation {
+
+    // In package bobsrockets.navigation
+    class Navigation
+
+    package tests {
+
+      // In package bobsrockets.navigation.tests
+      class NavigatorSuite
+    }
+  }
 }
