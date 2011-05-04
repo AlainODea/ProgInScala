abea81a007a286f717c7dfb7904832eab764dd86
Alain O'Dea <alain@alain-laptop.(none)>
2011-05-03 21:45:04 -0230

    Chapter 13 in progress
     - Nested packages and scopes

--- src/main/scala/bobsrockets.scala
+++ src/main/scala/bobsrockets.scala
@@ -2,12 +2,20 @@ package bobsrockets {
   package navigation {
 
     // In package bobsrockets.navigation
-    class Navigation
-
-    package tests {
-
-      // In package bobsrockets.navigation.tests
-      class NavigatorSuite
+    class Navigator {
+      // No need to say bobsrockets.navigation.StarMap
+      val map = new StarMap
+    }
+    class StarMap
+  }
+  class Ship {
+    // No need to say bobsrockets.navigation.Navigator
+    val nav = new navigation.Navigator
+  }
+  package fleets {
+    class Fleet {
+      // No need to say bobsrockets.Ship
+      def addShip() { new Ship }
     }
   }
 }
