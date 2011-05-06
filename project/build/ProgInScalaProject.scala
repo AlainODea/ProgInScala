7a7dd08a6db9e36743f64158496b8ae79e7ac3f1
Alain O'Dea <alain@alain-laptop.(none)>
2011-05-06 00:50:19 -0230

    Chapter 14 in progress
     - Use TestNG to implement Element tests both independently and with ScalaTest

--- project/build/ProgInScalaProject.scala
+++ project/build/ProgInScalaProject.scala
@@ -3,4 +3,5 @@ import sbt._
 class ProgInScalaProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
   val scalaTest = "org.scalatest" % "scalatest" % "1.3"
   val junit = "junit" % "junit" % "4.8.2"
+  val testng = "org.testng" % "testng" % "6.0.1"
 }
--- src/main/scala/ElementSuite.scala
+++ src/main/scala/ElementSuite.scala
@@ -1,10 +1,15 @@
-import org.scalatest.FunSuite
+import org.scalatest.testng.TestNGSuite
+import org.testng.annotations.Test
 import Element.elem
 
-class ElementSuite extends FunSuite {
+class ElementSuite extends TestNGSuite {
 
-  test("elem result should have passed width") {
+  @Test def verifyUniformElement() {
     val ele = elem('x', 2, 3)
-    assert(ele.width == 2)
+    assert(ele.width === 2)
+    expect(3) { ele.height }
+    intercept[IllegalArgumentException] {
+      elem('x', -2, 3)
+    }
   }
 }
