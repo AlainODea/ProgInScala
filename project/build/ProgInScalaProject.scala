d743fa30af221466a9fd40956a4fcf4cbd1e87fd
Alain O'Dea <alain@alain-laptop.(none)>
2011-05-06 00:24:49 -0230

    Chapter 14 in progress
     - Add JUnit 4.8.2 dependency in SBT (loving SBT :))
    
     - Use JUnit to implement a TestCase in Scala

--- project/build/ProgInScalaProject.scala
+++ project/build/ProgInScalaProject.scala
@@ -2,4 +2,5 @@ import sbt._
 
 class ProgInScalaProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
   val scalaTest = "org.scalatest" % "scalatest" % "1.3"
+  val junit = "junit" % "junit" % "4.8.2"
 }
