ee1cfebdb38037e31e334ee376e84e634550f3b2
Alain O'Dea <alain.odea@gmail.com>
2011-05-24 11:20:33 -0230

    Chapter 14 Complete (with caveats)
     - Could not get specs and ScalaCheck examples running (no Internet)
    
     - Did not add SBT dependency for ScalaCheck (no Internet to find what it would be)

--- project/build/ProgInScalaProject.scala
+++ project/build/ProgInScalaProject.scala
@@ -1,7 +1,8 @@
 import sbt._
 
 class ProgInScalaProject(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
-  val scalaTest = "org.scalatest" % "scalatest" % "1.3"
-  val junit = "junit" % "junit" % "4.8.2"
-  val testng = "org.testng" % "testng" % "6.0.1"
+  val scalaTest = "org.scalatest" % "scalatest" % "1.3" withSources()
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.7.2" withSources()
+  val junit = "junit" % "junit" % "4.8.2" withSources()
+  val testng = "org.testng" % "testng" % "6.0.1" withSources()
 }
